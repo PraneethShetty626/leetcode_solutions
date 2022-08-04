20033334e2b6c5540d1c2dcb16780f71a778eba8
unknown <praneethshetty626@gmail.com>
2022-08-04 22:23:45 +0530

    prob11

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String[] hs=sc.nextLine().split(" ");
+        int[] height=new int[hs.length];
+        for(int i=0;i<hs.length;i++){
+            height[i]=Integer.parseInt(hs[i]);
+        }
+
+        //
+        Problem11 p=new Problem11();
+        System.out.println(p.max_two(height));
+        System.out.println(p.maxArea(height));
+
+
+    }
+}
--- Problem11.java
+++ Problem11.java
@@ -1,5 +1,51 @@
+
+
 class Problem11{
-    public static void main(String[] args) {
-        
-    }
+
+    
+        public int maxArea(int[] height) {
+            int area=0;
+            for(int i=0;i<height.length;i++){
+                for(int j=i+1;j<height.length;j++){
+                    int w=j-i;
+                    int h=Math.min(height[i], height[j]);
+
+                    int a=w*h;
+
+                    if(a>area){
+                        area=a;
+                    }
+                }
+            }
+            return area;
+        }
+
+        //Two poinet approch
+
+        public int max_two(int[] height){
+            int area=0;
+
+            int left=0;
+            int right=height.length-1;
+   
+            while(left<right){
+                int w=right-left;
+                int h=Math.min(height[left], height[right]);
+
+                int a=w*h; 
+
+                if(a>area){
+                    area=a;
+                }
+                if(height[left]>height[right]){
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+
+            return area;
+        }
+    
 }
