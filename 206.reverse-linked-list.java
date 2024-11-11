d280f62eb8f9c3d2a48dfbe15436e6264bc6b55b
Praneeth Shetty <praneeth.shetty@mobileum.com>
2024-11-11 21:02:45 +0530

    206.reverse-linked-list.java

---
+++ 206.reverse-linked-list.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=206 lang=java
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        return reverse(head, null);
+    }
+
+    public ListNode reverse(ListNode head, ListNode prev) {
+
+            if(head == null) {
+                return prev;
+            }
+
+            ListNode temp  = prev ;
+            ListNode next = head.next;
+
+            head.next = temp;
+
+        
+            return reverse(next, head);
+        
+    }
+}
+// @lc code=end
+
