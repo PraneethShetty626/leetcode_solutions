b62eebe22f708fe88f94d5b07ddf6d8ff59f3217
PraneethShetty626 <praneethshetty626@gmail.com>
2024-11-11 22:36:10 +0530

    Unoptimal 292.nim-game.java

---
+++ 292.nim-game.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=292 lang=java
+ *
+ * [292] Nim Game
+ */
+
+// @lc code=start
+class Solution {
+    public boolean canWinNim(int n) {
+        return canWin(n, true, 1) || canWin(n, true, 2) || canWin(n, true, 3);
+    }
+
+    private boolean canWin(int n , boolean player, int steps) {
+       if(steps == n) {
+        return true;
+       }
+       else if (steps > n) {
+        return false;
+       }
+
+       boolean step1 = canWin(n, !player, steps + 1);
+       boolean step2 = canWin(n, !player, steps + 2);
+       boolean step3 = canWin(n, !player, steps + 3);
+
+       return !(step1 || step2 || step3 );
+
+
+    }
+
+}
+// @lc code=end
+
