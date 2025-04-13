4b5eaade8a43584a9bf52b2311f9c1fccb6d0975
Praneeth Shetty <praneethshetty626@gmail.com>
2025-04-13 11:32:23 +0530

    My sol

--- 48.rotate-image.java
+++ 48.rotate-image.java
@@ -69,5 +69,28 @@ class Solution {
             }
         }
     }
+
+    public void rotateBy180(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+    }
+
+    public void transposeMatrix(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
 }
 // @lc code=end
--- Main.java
+++ Main.java
@@ -1,8 +1,63 @@
+
 public class Main {
     public static void main(String[] args) {
-            int a ,b ;
+        int[] arr = new int[] { 2, 1, 5, 4, 3, 0, 0 };
 
-            System.out.println(a+b);
-            
+        nextPermutation(arr);
+    }
+
+    public static void nextPermutation(int[] nums) {
+
+        int maxIndex = getMaxIndex(nums);
+
+        if (maxIndex == nums.length - 1) {
+            reverseArray(nums, 0, nums.length - 1);
+        } else {
+
+            int smallestBefore = getSmallestIndex(nums, 0, maxIndex);
+            int smallestAfter = getSmallestIndexGreaterThan(nums, smallestBefore, maxIndex, nums.length - 1);
+
+            System.err.println(smallestAfter);
+
+        }
+
+    }
+
+    static void reverseArray(int[] arr, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    static int getMaxIndex(int[] nums) {
+        int maxIndex = nums.length - 1;
+
+        while (maxIndex > 0 && nums[maxIndex - 1] >= nums[maxIndex]) {
+            maxIndex--;
+        }
+        return maxIndex;
+    }
+
+    static int getSmallestIndex(int[] arr, int start, int end) {
+        while (start <= end - 1 && arr[start] > arr[start + 1]) {
+            start++;
+        }
+
+        return start;
+    }
+
+    static int getSmallestIndexGreaterThan(int[] arr, int item, int start, int end) {
+        int index = start;
+        int min = item;
+
+        while (start <= end - 1 && arr[start] > arr[start + 1]) {
+            if(arr[start] > item && arr[start] < ) 
+            start++;
+           
+        }
+
+        return start;
     }
 }
